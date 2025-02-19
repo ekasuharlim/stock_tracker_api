2b1e864c3bd2432f1a9e420c932d829b9de276c5
eka <eka@example.com>
2025-02-19 17:03:55 +0700

    Add logging

--- stock_tracker_api/Data/StockTrackingDbContext.cs
+++ stock_tracker_api/Data/StockTrackingDbContext.cs
@@ -4,9 +4,24 @@ using StockTrackingApi.Models;
 
 namespace StockTrackingApi.Data 
 {
+    
     public class StockTrackingDbContext : DbContext {
+        private readonly ILogger<StockTrackingDbContext> _logger;
 
-        public StockTrackingDbContext(DbContextOptions<StockTrackingDbContext> options) : base (options) {}
+        public StockTrackingDbContext(DbContextOptions<StockTrackingDbContext> options, ILogger<StockTrackingDbContext> logger) 
+        : base (options) {
+            _logger = logger;
+            _logger.LogInformation("StockTrackingDbContext initialized.");            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                _logger.LogWarning("DbContext is not properly configured!");
+            }
+        }        
+        
         public DbSet<User> Users { get; set; }
         public DbSet<Stock> Stocks { get; set; }
 
--- stock_tracker_api/Program.cs
+++ stock_tracker_api/Program.cs
@@ -2,9 +2,20 @@ using Microsoft.EntityFrameworkCore;
 using StockTrackingApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
 
 string? connectionString  = builder.Configuration.GetConnectionString("stockApiDb");
+var logger = LoggerFactory.Create(
+    logging => {
+        logging.AddConsole();
+        logging.AddDebug();
+        logging.SetMinimumLevel(LogLevel.Debug);
+    }).CreateLogger("Program");
 
+logger.LogDebug("DEBUG: Application is starting...");
+logger.LogDebug($"Using Connection String: {connectionString}");
 
 builder.Services.AddDbContext<StockTrackingDbContext>(
     options => options.UseSqlServer(connectionString)
@@ -15,6 +26,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+var appLogger = app.Logger;
+app.Use(async (context, next) =>
+{
+    appLogger.LogInformation($"Handling request: {context.Request.Method} {context.Request.Path}");
+    await next();
+    appLogger.LogInformation($"Response status: {context.Response.StatusCode}");
+});
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
